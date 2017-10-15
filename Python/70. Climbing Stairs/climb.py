e853e7f1a9ae2921ef9ccdd3e96d53b0bf6aa3e8
Wenbo <ws3109@g.rit.edu>
2017-10-15 00:32:42 -0400

    Create climb.py

---
+++ Python/70. Climbing Stairs/climb.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [i for i in range(n+1)]
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+        
