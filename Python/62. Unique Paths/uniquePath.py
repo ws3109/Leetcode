a420f0f82e1fea62ba21fa6ae6ba3e099370d1e6
Wenbo <ws3109@g.rit.edu>
2017-10-15 00:54:37 -0400

    Create uniquePath.py

---
+++ Python/62. Unique Paths/uniquePath.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+        
