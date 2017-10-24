e418906477d9e01b86abf42dcc872ecc5fee4d45
Wenbo <ws3109@g.rit.edu>
2017-10-24 00:05:56 -0400

    Create max_binary_tree.py

---
+++ Python/654. Maximum Binary Tree/max_binary_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+        max_val = max(nums)
+        index = nums.index(max_val)
+        root = TreeNode(max_val)
+        root.left = self.constructMaximumBinaryTree(nums[:index])
+        root.right = self.constructMaximumBinaryTree(nums[index+1:])
+        return root
