50f0df1cfd44f991ab6fdc52244eaa791b811285
Wenbo <ws3109@g.rit.edu>
2017-10-23 23:45:23 -0400

    Create pigs.py

---
+++ Python/458. Poor Pigs/pigs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        return int(math.ceil(math.log(buckets, minutesToTest//minutesToDie + 1)))
+        
