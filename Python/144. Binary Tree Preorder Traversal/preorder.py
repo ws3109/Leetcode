7770a9b523588d2881783ca5dd6a754ce20beb27
Wenbo <ws3109@g.rit.edu>
2017-10-25 12:49:26 -0400

    Create preorder.py

---
+++ Python/144. Binary Tree Preorder Traversal/preorder.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        preorder_result = []
+        stack = []
+        while root or stack:
+            if root:
+                preorder_result.append(root.val)
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                root = root.right 
+        return preorder_result
+                
