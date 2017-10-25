4ac7cdc14c3776642d03aaaff682021ad033fa9d
Wenbo <ws3109@g.rit.edu>
2017-10-24 21:12:45 -0400

    Create lcs.py

---
+++ Python/14. Longest Common Prefix/lcs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        elif len(strs) == 1:
+            return strs[0]
+        else:
+            return self.LCS(strs[0], self.longestCommonPrefix(strs[1:]))
+    
+    def LCS(self, s1, s2):
+        i = 0
+        while i < len(s1) and i < len(s2) and s1[i] == s2[i]:
+            i += 1
+        return s1[:i]
