a3a66f0b26aaf60caf3942f9572388a12cb3d4ac
Wenbo <ws3109@g.rit.edu>
2017-10-28 19:16:16 -0400

    Create fast.py

---
+++ Python/240. Search a 2D Matrix II/fast.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+        return False
+        
